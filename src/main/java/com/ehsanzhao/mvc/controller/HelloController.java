3973859e99ce6525647f143ae93b4d3d8d71b1fb
zhaoyuan <me@ehsanzhao.com>
2023-01-06 15:10:45 +0800

    first commit

---
+++ src/main/java/com/ehsanzhao/mvc/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.ehsanzhao.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zhaoyuan
+ * @date 2023/1/5
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
+
+}
